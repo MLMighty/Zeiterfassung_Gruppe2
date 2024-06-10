e4450c62141ac0dc392f3f9eff55cb430413017a
Nicu-T <nicolae.topala@ing.de>
2024-06-10 10:50:34 +0200

    Scanner

--- Zeiterfassung/src/Main.java
+++ Zeiterfassung/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         for (int i=0; i<20;i++){
             System.out.println("Hello World");
             System.out.println("Hello World");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a number");
+            int number = scanner.nextInt();
+            System.out.println("Please enter another number");
+            int number2 = scanner.nextInt();
+            if (number>number2){
+                System.out.println("You are greater than the number");
+                scanner.close();
+            }
         }
     }
 }
