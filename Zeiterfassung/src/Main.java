5aeae61deb708b59092f51a3f0a4cfef36a15d37
T104P19A <nicolae.topala@ing.de>
2024-06-10 15:15:40 +0200

    Database Connection X

--- Zeiterfassung/src/Main.java
+++ Zeiterfassung/src/Main.java
@@ -1,20 +1,8 @@
-import java.util.Scanner;
+package Zeiterfassung.src;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i=0; i<20;i++){
-            System.out.println("Hello World");
-            System.out.println("Hello World");
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter a number");
-            int number = scanner.nextInt();
-            System.out.println("Please enter another number");
-            int number2 = scanner.nextInt();
-            if (number>number2){
-                System.out.println("You are greater than the number");
-                scanner.close();
-                
-            }
-        }
+        DatabaseConnection db = new DatabaseConnection(); // Instanzierung der DatabaseConnection-Klasse
+        db.connectToDatabase("s27sql02.schulung-provadis.de", "admin_02", "L_$ZCw43ik47");
     }
 }
