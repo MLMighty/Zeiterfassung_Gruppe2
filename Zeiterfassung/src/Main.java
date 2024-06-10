cb44ba674ed50b5a603c6ef24a987e005aae0899
T104P19A <nicolae.topala@ing.de>
2024-06-10 15:23:51 +0200

    Delete All

--- Zeiterfassung/src/Main.java
+++
@@ -1,8 +0,0 @@
-package Zeiterfassung.src;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseConnection db = new DatabaseConnection(); // Instanzierung der DatabaseConnection-Klasse
-        db.connectToDatabase("s27sql02.schulung-provadis.de", "admin_02", "L_$ZCw43ik47");
-    }
-}
