a53e489d7753abfda08eac703515716a24397034
T104P14A <T104P14A@schulung-provadis.de>
2024-06-12 10:47:59 +0200

    Authentification Controller finished

---
+++ src/main/java/com/crispy_wombats/time_entry/controller/AdminController.java
@@ -0,0 +1,5 @@
+package com.crispy_wombats.time_entry.controller;
+
+public class AdminController {
+
+}
---
+++ src/main/java/com/crispy_wombats/time_entry/controller/TimeEntryController.java
@@ -0,0 +1,9 @@
+package com.crispy_wombats.time_entry.controller;
+
+public class TimeEntryController {
+    //eig
+        //
+    //constr
+    //meth
+    //getset
+}
