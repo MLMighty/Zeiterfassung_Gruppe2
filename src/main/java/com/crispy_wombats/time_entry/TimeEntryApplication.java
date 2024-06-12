e3a477ba09b2b69c16732da2558ce2435812f46f
T104P16A <T104P16A@schulung-provadis.de>
2024-06-12 08:08:00 +0200

    Spring Porject

---
+++ src/main/java/com/crispy_wombats/time_entry/TimeEntryApplication.java
@@ -0,0 +1,13 @@
+package com.crispy_wombats.time_entry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeEntryApplication.class, args);
+	}
+
+}
