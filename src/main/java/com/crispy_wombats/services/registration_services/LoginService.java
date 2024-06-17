b3f73d1a4e3b7bc5fd41de6ad5067f493f9ba6d0
T104P14A <T104P14A@schulung-provadis.de>
2024-06-17 14:35:37 +0200

    fisch

--- src/main/java/com/crispy_wombats/services/registration_services/LoginService.java
+++ src/main/java/com/crispy_wombats/services/registration_services/LoginService.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class LoginService {
 
