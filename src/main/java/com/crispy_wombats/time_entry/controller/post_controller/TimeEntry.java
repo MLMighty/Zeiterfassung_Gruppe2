4e4a8323bc000bd807d7b96991cf10010664a750
Your Name <you@example.com>
2024-06-18 09:56:08 +0200

    d

--- src/main/java/com/crispy_wombats/time_entry/controller/post_controller/TimeEntry.java
+++ src/main/java/com/crispy_wombats/time_entry/controller/post_controller/TimeEntry.java
@@ -1,6 +1,8 @@
 package com.crispy_wombats.time_entry.controller.post_controller;
 
 
+import com.crispy_wombats.time_entry.models.TaskModel;
+import com.crispy_wombats.time_entry.models.TimeModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500",allowCredentials = "true")
+@CrossOrigin(origins = "http://127.0.0.1:5501",allowCredentials = "true")
 @RestController
 public class TimeEntry {
 
      @PostMapping("/timeentry")
      public void absenceController (@RequestBody  Map<String,Object> timeEntryData  ){
+         TaskModel taskModel = new TaskModel();
+         String fisch = (String) timeEntryData.get("taskname");
+         System.out.println(fisch);
+         /*taskModel.setTaskname();
+         TimeModel timeModel = new TimeModel();*/
 
 
 
