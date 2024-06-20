c30c0163f8c26471261f81a70dcd5d590b04e8c9
Your Name <you@example.com>
2024-06-20 12:53:15 +0200

    fd

--- src/main/java/com/crispy_wombats/repositorys/UserRepository.java
+++ src/main/java/com/crispy_wombats/repositorys/UserRepository.java
@@ -1,10 +1,19 @@
 package com.crispy_wombats.repositorys;
 import com.crispy_wombats.models.UsersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UsersModel, Integer> {
     UsersModel findByEmail(String email);
     UsersModel findAllByPassword(String password);
+
+    @Query(value = "SELECT dbo.ufGetUserID(:uuid)", nativeQuery = true)
+    Integer callUfGetUserID(@Param("uuid") UUID uuid);
 }
