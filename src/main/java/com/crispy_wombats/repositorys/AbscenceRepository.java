8bcf443088c4e75e45dd8425c8dcfc5f78a9d68e
Your Name <you@example.com>
2024-06-20 15:07:29 +0200

    bug fixed

--- src/main/java/com/crispy_wombats/repositorys/AbscenceRepository.java
+++ src/main/java/com/crispy_wombats/repositorys/AbscenceRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface AbscenceRepository extends JpaRepository<AbsenceModel, Integer> {
-    List<AbsenceModel> findByUserId(int userId);
+   /* List<AbsenceModel> findByUserId(int userId);*/
 }
